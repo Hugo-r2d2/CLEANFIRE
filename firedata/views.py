aa8814ba8f9af83dd17d0cb1d305c2c30276297d
Hugo Camelo dos Santos <ihugocs@gmail.com>
2024-11-13 16:47:29 -0400

    connect Dynamodb

--- firedata/views.py
+++ firedata/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.conf import settings 
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from collections import defaultdict
+import pandas as pd 
+import boto3
+import time
 
-# Create your views here.
+# Função que conecta o DynamoDB AWS
+def conectar_dynamodb():
+    return boto3.resource(
+        'dynamodb',
+        region_name = 'us-east-1',
+        aws_access_key_id = settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key = settings.AWS_SECRET_ACCESS_KEY
+    )
+
